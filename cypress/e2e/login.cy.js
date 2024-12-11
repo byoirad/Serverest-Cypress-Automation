841f57312b819a1fe9e754d48da7f1071fd9e924
Dário B. de Brito <dario.barbosabr@gmail.com>
2024-12-11 09:16:51 -0300

    :test_tube: test: AdicAdicionando novos testes de login + API

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,19 +2,37 @@ import LoginPage from "../support/pages/loginPage"
 
 describe('Dado que estou na página de login', function () {
 
-  context('Quando preencho os campos com dados de usuário NORMAL válidos', function () {
+  beforeEach(function () {
+    cy.fixture('users').then(function (user) {
+      this.user = user
+    })
+  })
 
-    const data = {
-      nome: 'Teste da Silva',
-      email: 'teste_dasilva@gmail.com',
-      password: 'testedasilvasenha'
-    }
+  context('Quando preencho os campos com dados válidos', function () {
 
-    it('Então o sitema deve logar com sucesso', function () {
+    it('Então o sistema deve fazer login de usuário NORMAL com sucesso', function () {
+
+      const user = this.user.normal_user
+
+      cy.deleteByEmail(user.email)
+      cy.createUserApi(user)
 
       LoginPage.go()
-      LoginPage.fillLoginForm(data)
-      LoginPage.loginFormSubmit()
+      LoginPage.fillLoginForm(user)
+      LoginPage.loginFormSubmitNormal()
     })
+
+    it('Então o sistema deve fazer login de usuário ADMIN com sucesso', function () {
+
+      const user = this.user.admin_user
+
+      cy.deleteByEmail(user.email)
+      cy.createUserApi(user)
+
+      LoginPage.go()
+      LoginPage.fillLoginForm(user)
+      LoginPage.loginFormSubmitAdmin()
+    })
+
   })
 })
